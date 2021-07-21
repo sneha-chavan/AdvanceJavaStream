976be1bc142802c5df052ada7217c2b8d09df993
sneha-chavan <snehachavan115@gmail.com>
2021-07-22 00:44:52 +0530

    welcome to hotel reservation system

---
+++ src/main/HotelReservation.java
@@ -0,0 +1,8 @@
+package main;
+
+public class HotelReservation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation Program");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module hotelreservation {
+}
